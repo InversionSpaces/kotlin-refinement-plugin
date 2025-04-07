bb55dce5f04257dfed431ccb63d37d16a5947a85
InversionSpaces <inversionspaces@vivaldi.net>
2025-04-07 13:09:10 +0200

    Render graph

--- src/main/kotlin/com/example/refinement/Diagnostics.kt
+++ src/main/kotlin/com/example/refinement/Diagnostics.kt
@@ -1,16 +1,23 @@
 package com.example.refinement
 
+import com.example.refinement.RefinementDiagnostics.CFG_GRAPH
 import com.example.refinement.RefinementDiagnostics.CONTAINING_DECLARATION
+import com.example.refinement.RefinementDiagnostics.NO_CONTAINING_DECLARATION
 import org.jetbrains.kotlin.com.intellij.psi.PsiElement
 import org.jetbrains.kotlin.diagnostics.KtDiagnosticFactoryToRendererMap
 import org.jetbrains.kotlin.diagnostics.rendering.BaseDiagnosticRendererFactory
+import org.jetbrains.kotlin.diagnostics.rendering.CommonRenderers
 import org.jetbrains.kotlin.diagnostics.rendering.RootDiagnosticRendererFactory
+import org.jetbrains.kotlin.diagnostics.warning0
 import org.jetbrains.kotlin.diagnostics.warning1
 import org.jetbrains.kotlin.fir.analysis.diagnostics.FirDiagnosticRenderers
 import org.jetbrains.kotlin.fir.symbols.FirBasedSymbol
 
 object RefinementDiagnostics {
     val CONTAINING_DECLARATION by warning1<PsiElement, FirBasedSymbol<*>>()
+    val NO_CONTAINING_DECLARATION by warning0<PsiElement>()
+
+    val CFG_GRAPH by warning1<PsiElement, String>()
 
     init {
         RootDiagnosticRendererFactory.registerFactory(RefinementDiagnosticRender)
@@ -21,5 +28,7 @@ object RefinementDiagnosticRender : BaseDiagnosticRendererFactory() {
     override val MAP: KtDiagnosticFactoryToRendererMap
         get() = KtDiagnosticFactoryToRendererMap("Refinement").apply {
             put(CONTAINING_DECLARATION, "Containing declaration: {0}", FirDiagnosticRenderers.DECLARATION_NAME)
+            put(NO_CONTAINING_DECLARATION, "No containing declaration found")
+            put(CFG_GRAPH, "CFG graph: {0}", CommonRenderers.STRING)
         }
 }
--- src/main/kotlin/com/example/refinement/FirRefinementAdditionalCheckersExtension.kt
+++ src/main/kotlin/com/example/refinement/FirRefinementAdditionalCheckersExtension.kt
@@ -1,20 +1,29 @@
 package com.example.refinement
 
 import com.example.refinement.RefinementDiagnostics.CONTAINING_DECLARATION
+import com.example.refinement.RefinementDiagnostics.NO_CONTAINING_DECLARATION
 import org.jetbrains.kotlin.com.intellij.psi.PsiElement
 import org.jetbrains.kotlin.diagnostics.DiagnosticReporter
 import org.jetbrains.kotlin.diagnostics.error0
 import org.jetbrains.kotlin.diagnostics.reportOn
 import org.jetbrains.kotlin.diagnostics.warning0
 import org.jetbrains.kotlin.fir.FirSession
+import org.jetbrains.kotlin.fir.analysis.cfa.nearestNonInPlaceGraph
+import org.jetbrains.kotlin.fir.analysis.cfa.util.traverseToFixedPoint
 import org.jetbrains.kotlin.fir.analysis.checkers.MppCheckerKind
 import org.jetbrains.kotlin.fir.analysis.checkers.context.CheckerContext
+import org.jetbrains.kotlin.fir.analysis.checkers.context.findClosest
 import org.jetbrains.kotlin.fir.analysis.checkers.expression.ExpressionCheckers
 import org.jetbrains.kotlin.fir.analysis.checkers.expression.FirCallChecker
 import org.jetbrains.kotlin.fir.analysis.diagnostics.FirErrors
 import org.jetbrains.kotlin.fir.analysis.extensions.FirAdditionalCheckersExtension
+import org.jetbrains.kotlin.fir.declarations.FirControlFlowGraphOwner
+import org.jetbrains.kotlin.fir.declarations.FirFunction
 import org.jetbrains.kotlin.fir.expressions.FirCall
 import org.jetbrains.kotlin.fir.expressions.arguments
+import org.jetbrains.kotlin.fir.resolve.dfa.LogicSystem
+import org.jetbrains.kotlin.fir.resolve.dfa.cfg.render
+import org.jetbrains.kotlin.fir.resolve.dfa.controlFlowGraph
 
 class FirRefinementAdditionalCheckersExtension(session: FirSession): FirAdditionalCheckersExtension(session) {
     override val expressionCheckers: ExpressionCheckers = object : ExpressionCheckers() {
@@ -29,8 +38,14 @@ class FirRefinementAdditionalCheckersExtension(session: FirSession): FirAddition
                 context: CheckerContext,
                 reporter: DiagnosticReporter
             ) {
-                val declaration = context.containingDeclarations.last()
+                val declaration = context.findClosest<FirFunction>()
+                if (declaration == null) {
+                    return reporter.reportOn(expression.source, NO_CONTAINING_DECLARATION, context)
+                }
                 reporter.reportOn(expression.source, CONTAINING_DECLARATION, declaration.symbol, context)
+
+                val cfg = declaration.controlFlowGraphReference?.controlFlowGraph ?: return
+                reporter.reportOn(expression.source, RefinementDiagnostics.CFG_GRAPH, cfg.render(), context)
             }
         }
     }
